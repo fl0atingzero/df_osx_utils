30a0fd2249f4833ae939a41074709898821aa1d6
Vladislav Brezgin <brezgin.vl@yandex.ru>
2022-02-20 12:28:43 +0300

    moving some scripts to obsolete dir

---
+++ README.md
@@ -0,0 +1,10 @@
+Doom 2D: Forever utils for OS X
+===============================
+
+This is small collection of utilities for building Doom 2D: Forever for OS X (darwin)
+
+- **builder.sh** - script for semi-automatic building Doom 2D on OS X natively
+- **macportsutil.sh** - script for automatic downloading and unpacking libraries from MacPorts (useful for cross-compilation)
+- **obsolete** - directory for some other scripts, that are not necessary now for my purposes
+
+by fl0atingzero
---
+++ macportsutil.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#this script is for getting URL links to download specific links from MacPorts for specific OS X, downloading and extracting them
+
+OUTDIR="/opt/local/"
+URL="https://packages.macports.org"
+LIBS=("libsdl2"
+      "libsdl2_mixer"
+      "libmodplug"
+      "libvorbis"
+      "libogg"
+      "flac"
+      "mpg123"
+      "opusfile"
+      "libopus")
+DARWINVER="12"
+ARCH="x86_64"
+
+PATTERN="darwin_$DARWINVER\.$ARCH"
+
+LIBFNAMES=()
+
+echo "This script will help you to obtain specific libs from MacPorts' repository" #. Press y to continue..."
+#read -s -n 1 key
+#if ! [ "$key" = "y" ]; then
+#   exit 0
+#fi
+
+#echo "OK, proceeding"
+echo "Getting libs' links"
+echo ""
+
+for LIBNAME in ${LIBS[*]}
+do
+    # this horrifying thing actually parses macports' download page and find required link
+
+    ANSWER=$( curl -s "$URL/$LIBNAME/" | grep -Eo "$LIBNAME[^>]+\.$PATTERN\.tbz2" | grep -v "universal" | tr " " "\n" | sort | uniq | tail -n 1 )
+
+    # there is another way to get required library name which seems to be more complicated
+
+    #ANSWER=$( curl -s "$URL" | grep -Eo "(href=\")[^>]+($PATTERN.tbz2\")>" | grep -v "universal" | tr " " "\n" | sort | uniq | tail -n 1 | cut -c 7- | rev | cut -c 3- | rev )
+    # curl -s "$URL" - we'll get page where links are listed
+    # grep -Eo '(href=\")[^>]+($PATTERN.tbz2\")>' - then we'll parse links that meets our pattern
+    # grep -v "universal" - we'll also exclude universal binaries because we don't need them
+    # tr " " "\n" - replace whitespace with newline characters
+    # sort - sort strings
+    # uniq - remove duplicate strings
+    # tail -n 1 - remove all but last string
+    # cut -c 7- | rev - cut first 7 characters then reverse string
+    # cut -c 3- | rev - cut first (actually last) 3 characters then reverse string to get final answer
+    LIBFNAMES+=("$ANSWER")
+    echo $LIBNAME: $URL/$LIBNAME/$ANSWER
+done
+
+echo ""
+if [ -d "$OUTDIR/libs" ]; then
+    echo "$OUTDIR/libs directory already exists, trying to delete it"
+    rm -rv $OUTDIR/libs
+fi
+
+mkdir $OUTDIR
+
+echo ""
+echo "Now downloading and extracting libs..."
+cd $OUTDIR
+
+for i in ${!LIBS[@]}
+do
+    echo ""
+    echo "-->${LIBS[$i]}"
+    curl -O $URL/${LIBS[$i]}/${LIBFNAMES[$i]}
+    echo ""
+    echo "extracting ${LIBS[$i]}..."
+    tar --strip-components=3 -xjvf ${LIBFNAMES[$i]} ./opt/local/lib
+done
